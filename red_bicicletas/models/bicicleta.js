9cb1b7171169589c94e3d3b9f21e58e075e26913
AndresGarcia98 <garciaagudelod@gmail.com>
2020-08-23 19:21:31 -0500

    Se tiene todos los cambios del video IMV

--- red_bicicletas/models/bicicleta.js
+++ red_bicicletas/models/bicicleta.js
@@ -11,21 +11,29 @@ Bicicleta.prototype.toString = function() {
 
 Bicicleta.allBicis = [];
 Bicicleta.add = function(aBici) {
-    Bicicleta.allBicis.push((aBici)); 
+    Bicicleta.allBicis.push((aBici));
 }
 
-Bicicleta.findById = function(aBiciId){
-    var aBici = Bicicleta.allBicis.find[x => x.if == aBiciId]
+Bicicleta.findById = function(aBiciId) {
+    var aBici = Bicicleta.allBicis.find(x => x.id == aBiciId);
 
-    if(aBici){
+    if (aBici) {
         return aBici
     } else {
-        throw new Error(`No se encentra una bicicleta con el ID:$(aBiciId)`);
+        throw new Error(`No se encentra una bicicleta con el ID:${aBiciId}`);
     }
 }
 
+Bicicleta.removeById = function(aBiciID) {
+    for (var i = 0; i < Bicicleta.allBicis.length; i++) {
+        if (Bicicleta.allBicis[i].id == aBiciID) {
+            Bicicleta.allBicis.splice(i, 1);
+            break;
+        }
+    }
+}
 var a = new Bicicleta(1, 'rojo', 'urbana', [-34.6012424, -58.38614971]);
-var b = new Bicicleta(2, 'blanca', 'urbana', [-34.596932, -58.3808287]); 
+var b = new Bicicleta(2, 'blanca', 'urbana', [-34.596932, -58.3808287]);
 
 Bicicleta.add(a);
 Bicicleta.add(b);
