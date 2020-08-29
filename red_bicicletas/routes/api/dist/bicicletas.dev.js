d6d699447e41eaa2b5c673be692d327c4426d229
AndresGarcia98 <garciaagudelod@gmail.com>
2020-08-28 20:22:13 -0500

    API de Bicicletas

---
+++ red_bicicletas/routes/api/dist/bicicletas.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var express = require('express');
+
+var router = express.Router();
+
+var bicicletaController = require('../../controllers/api/bicicletaControllerAPI');
+
+router.get('/', bicicletaController.bicicleta_list);
+module.exports = router;
